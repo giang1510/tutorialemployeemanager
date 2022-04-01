1e7a9b200b3921a100871a5790cf0a6c90026664
giang phi <giang1510@hotmail.de>
2022-04-01 14:26:15 +0200

    Initial commit

---
+++ src/main/java/com/fixeo/tutorialemployeemanager/TutorialemployeemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.fixeo.tutorialemployeemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialemployeemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialemployeemanagerApplication.class, args);
+	}
+
+}
