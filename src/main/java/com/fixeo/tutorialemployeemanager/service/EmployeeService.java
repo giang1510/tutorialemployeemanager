7068ebb805022240ed7dbaa7b27c038b93a65171
giang phi <giang1510@hotmail.de>
2022-04-04 15:01:03 +0200

    Add JPA repository and corresponding service to access employee data

---
+++ src/main/java/com/fixeo/tutorialemployeemanager/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.fixeo.tutorialemployeemanager.service;
+
+import com.fixeo.tutorialemployeemanager.model.Employee;
+import com.fixeo.tutorialemployeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+}
