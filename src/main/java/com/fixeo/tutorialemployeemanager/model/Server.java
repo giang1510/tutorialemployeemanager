9673065ef175dd91ac9427c9eb6c5dff27254272
giang phi <giang1510@hotmail.de>
2022-04-26 14:03:18 +0200

    Add new model for data table "Server" (new) and new enum for server status field

---
+++ src/main/java/com/fixeo/tutorialemployeemanager/model/Server.java
@@ -0,0 +1,26 @@
+package com.fixeo.tutorialemployeemanager.model;
+
+import com.fixeo.tutorialemployeemanager.enumeration.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Server {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(unique = true)
+    @NotEmpty(message = "IP Address can not be empty or null")
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imgUrl;
+    private Status status;
+}
