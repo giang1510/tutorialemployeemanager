a3022ee3364d6de0983ede6f193da26f485639a0
giang phi <giang1510@hotmail.de>
2022-05-02 15:02:42 +0200

    Add test server data (commented code) and fix minor bugs: - img not found (wrong path) - unique ip adress

--- src/main/java/com/fixeo/tutorialemployeemanager/TutorialemployeemanagerApplication.java
+++ src/main/java/com/fixeo/tutorialemployeemanager/TutorialemployeemanagerApplication.java
@@ -1,5 +1,9 @@
 package com.fixeo.tutorialemployeemanager;
 
+import com.fixeo.tutorialemployeemanager.enumeration.Status;
+import com.fixeo.tutorialemployeemanager.model.Server;
+import com.fixeo.tutorialemployeemanager.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +12,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Arrays;
+
+import static com.fixeo.tutorialemployeemanager.enumeration.Status.SERVER_DOWN;
+import static com.fixeo.tutorialemployeemanager.enumeration.Status.SERVER_UP;
 
 @SpringBootApplication
 public class TutorialemployeemanagerApplication {
@@ -36,5 +43,15 @@ public class TutorialemployeemanagerApplication {
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
 		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
+
+	@Bean
+	CommandLineRunner run(ServerRepo serverRepo){
+		return args -> {
+//			serverRepo.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 GB", "Personal PC", "http://localhost:8080/server/img/server1.png", SERVER_UP));
+//			serverRepo.save(new Server(null, "192.168.1.58", "Fedora Linux", "16 GB", "Dell Tower","http://localhost:8080/server/img/server2.png", SERVER_DOWN));
+//			serverRepo.save(new Server(null, "192.168.1.21", "MS 2008", "32 GB", "Web Server", "http://localhost:8080/server/img/server3.png", SERVER_UP));
+//			serverRepo.save(new Server(null, "192.168.1.14", "Red Hat Enterprise Linux", "64 GB", "Mail Server", "http://localhost:8080/server/img/server4.png", SERVER_DOWN));
+		};
 	}
 }
--- src/main/java/com/fixeo/tutorialemployeemanager/resource/ServerResource.java
+++ src/main/java/com/fixeo/tutorialemployeemanager/resource/ServerResource.java
@@ -92,6 +92,6 @@ public class ServerResource {
 
     @GetMapping(path = "/img/{fileName}", produces = IMAGE_PNG_VALUE)
     public byte[] getServerImg(@PathVariable("fileName") String fileName) throws IOException {
-        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "Downloads/images/" + fileName));
+        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "/Downloads/images/" + fileName));
     }
 }
