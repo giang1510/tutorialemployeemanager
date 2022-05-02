6aa4e1bcb48ea5ece000c1f8188646e0b75d2e4a
giang phi <giang1510@hotmail.de>
2022-05-02 12:10:35 +0200

    Add controller ServerResource to map http-request onto data-access-method, in order to access server data

---
+++ src/main/java/com/fixeo/tutorialemployeemanager/resource/ServerResource.java
@@ -0,0 +1,97 @@
+package com.fixeo.tutorialemployeemanager.resource;
+
+import com.fixeo.tutorialemployeemanager.enumeration.Status;
+import com.fixeo.tutorialemployeemanager.model.Response;
+import com.fixeo.tutorialemployeemanager.model.Server;
+import com.fixeo.tutorialemployeemanager.service.implementation.ServerServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
+
+@RestController
+@RequestMapping("/server")
+@RequiredArgsConstructor
+public class ServerResource {
+    private final ServerServiceImpl serverService;
+
+    @GetMapping("/list")
+    public ResponseEntity<Response> getServers(){
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("servers", serverService.list(30)))
+                        .msg("Servers retrieved")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
+        Server server = serverService.ping(ipAddress);
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("server", server))
+                        .msg(server.getStatus() == Status.SERVER_UP ? "Ping success" : "Ping failed")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("server", serverService.create(server)))
+                        .msg("Server created")
+                        .status(HttpStatus.CREATED)
+                        .statusCode(HttpStatus.CREATED.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Response> getServer(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("server", serverService.get(id)))
+                        .msg("Server retrieved")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("deleted", serverService.delete(id)))
+                        .msg("Server deleted")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping(path = "/img/{fileName}", produces = IMAGE_PNG_VALUE)
+    public byte[] getServerImg(@PathVariable("fileName") String fileName) throws IOException {
+        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "Downloads/images/" + fileName));
+    }
+}
